2e54def20bccad4ef6c11685649f39d07510839c
2422527665@qq.com <2422527665@qq.com>
2018-11-26 07:42:02 +0000

    Add password reset & email configuration

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,12 +6,13 @@ use Illuminate\Http\Request;
 use App\Models\User;
 
 use Auth;
+use Mail;
 
 class UsersController extends Controller
 {
 	public function __construct(){
 		$this->middleware('auth',[
-			'except' => ['show','create','store','index']
+			'except' => ['show','create','store','index','confirmEmail']
 		]);
 
         $this->middleware('guest',[
@@ -46,9 +47,9 @@ class UsersController extends Controller
     		'password' => bcrypt($request->password),
     	]);
     	
-    	Auth::login($user);
-    	session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
-    	return redirect()->route('users.show',[$user]);
+    	$this->sendEmailConfirmationTo($user);
+    	session()->flash('success','验证信息以发送至你的邮箱，请前往确认激活！');
+    	return redirect('/');
     }
 
     public function edit(User $user){
@@ -79,5 +80,29 @@ class UsersController extends Controller
         $user->delete();
         session()->flash('success','用户删除成功！');
         return back();
+    }
+
+    public function confirmEmail($token){
+        $user = User::where('activation_token',$token)->firstOrFail();
+        $user->activated = true;
+        $user->activation_token = null;
+        $user->save();
+
+        Auth::login($user);
+        session()->flash('success','激活成功！');
+        return redirect()->route('users.show',[$user]);
+    }
+
+    protected function sendEmailConfirmationTo($user){
+        $view = 'email.confirm';
+        $data = compact('user');
+        $from = 'aufree@yousails.com';
+        $name = 'Aufree';
+        $to = $user->email;
+        $subject = '感谢注册 Sample 应用！请确认你的邮箱。';
+        Mail::send($view,$data,function ($message) use ($from,$name,$to,$subject){
+            $message->from($from,$name)->to($to)->subject($subject);
+        });
+
     }
 }
